bb234cb0c5611715c24824535f41830062b74d7c
samprasdsouza02 <sampras.dsouza@cimpress.com>
2023-10-27 17:39:53 +0530

    contact us form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Button from 'react-bootstrap/Button';
 import Header from './Header';
 import Footer from './Footer';
+import logo from './svg/second-img.png'
+import firstLogo from './svg/first-img.png'
 
 
 function App() {
@@ -9,11 +11,21 @@ function App() {
     <>
     <Header/>
     <div className="App">
-      <div className="d-flex justify-content-center align-items-center">PrimeSynth is a reliable supplier of reference standards, API impurities, R&D Chemicals & Custom synthesis products</div>
+      <div ><img src={logo} alt="logo" style={{ width: '1792px',height:'750px'}}/></div>
+      <div className="d-flex justify-content-center align-items-center" >PrimeSynth is a reliable supplier of reference standards, API impurities, R&D Chemicals & Custom synthesis products</div>
       <div className="d-flex justify-content-center align-items-center">Our Products</div>
       <div>No matter what you are searching for, we’ve got all the products you could possibly need. We ensure our clients receive only the best quality products for their analytical and research work. Check out our list of inventory below and feel free to contact us.Checkout our product catalogue or search in any products by CAT no., CAS no. or Product name </div>
       <div className='d-flex justify-content-center align-items-center'>
         <Button variant="primary" >Browse</Button>
+      </div>
+      <div>
+        <div><img src={firstLogo} alt="logo" style={{ width: '1792px',height:'750px'}}/></div>
+      </div>
+      <div className='d-flex justify-content-center align-items-center' >
+        <div>
+          <div>If you’re looking for a specific product and can’t find it. One of our staff members will be happy to assist you.</div>
+          <div className='d-flex justify-content-center align-items-center'> <Button type='primary'> Get in Touch</Button></div>
+        </div>
       </div>
     <Footer/>
     </div>
--- src/GetQuote.jsx
+++ src/GetQuote.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import Footer from "./Footer"
 import Header from "./Header"
 import { InputText } from "./common/InputText"
+import { Button } from "react-bootstrap"
 
 export const GetQuote = () => {
     const [QuoteForm, setQuoteForm] = useState({})
@@ -11,12 +12,11 @@ export const GetQuote = () => {
     }
 
     const handleInput = (event) => {
-        let {name, value} = event.target
+        let { name, value } = event.target
         console.log(name, value);
         handleChange(name, value)
     }
 
-    console.log(QuoteForm);
     return (
         <>
             <Header />
@@ -26,13 +26,85 @@ export const GetQuote = () => {
                 </div>
                 <div className="d-flex justify-content-center">
                     <span className="text-14">Get in touch with us to get an online quotation</span>
-                </div>  
-                <div className=" d-flex justify-content-center">
-                    <InputText
-                    name="test"
-                    value={QuoteForm?.test}
-                    onChange={handleInput}
-                    />
+                </div>
+                <div className="d-flex justify-content-center align-items-center">
+                    <div className="">
+                        <div className="d-flex">
+                            <div className="p-3">
+                                <InputText
+                                    name="firstName"
+                                    label="First Name"
+                                    required
+                                    value={QuoteForm?.firstName}
+                                    onChange={handleInput}
+                                />
+                            </div>
+                            <div className="p-3">
+                                <InputText
+                                    name="lastName"
+                                    label="Last Name"
+                                    required
+                                    value={QuoteForm?.lastName}
+                                    onChange={handleInput}
+                                />
+                            </div>
+                            <div className="p-3">
+                                <InputText
+                                    name="companyName"
+                                    label="Company Name"
+                                    required
+                                    value={QuoteForm?.companyName}
+                                    onChange={handleInput}
+                                />
+                            </div>
+                            <div className="p-3">
+                                <InputText
+                                    name="countryName"
+                                    label={"Country"}
+                                    value={QuoteForm?.countryName}
+                                    onChange={handleInput}
+                                />
+                            </div>
+                        </div>
+                        <div className="d-flex">
+                            <div className="p-3">
+                                <InputText
+                                    name="email"
+                                    label={"Email"}
+                                    value={QuoteForm?.email}
+                                    onChange={handleInput}
+                                />
+                            </div>
+                            <div className="p-3">
+                                <InputText
+                                    name="productName"
+                                    label={"Product Name"}
+                                    value={QuoteForm?.productName}
+                                    onChange={handleInput}
+                                />
+                            </div>
+                            <div className="p-3">
+                                <InputText
+                                    name="CAS"
+                                    label={"CAS Name"}
+                                    value={QuoteForm?.CAS}
+                                    onChange={handleInput}
+                                />
+                            </div>
+                            <div className="p-3">
+                                <InputText
+                                    name="CAT"
+                                    label={"CAT Name"}
+                                    value={QuoteForm?.CAT}
+                                    onChange={handleInput}
+                                />
+                            </div>
+
+                        </div>
+                        <div className="d-flex justify-content-center align-items-center">
+                            <Button type="primary" >Get Quote</Button>
+                        </div>
+                    </div>
                 </div>
             </div>
             <Footer />
